9274de179d904ddb7855278dec79e70ccfb24e9b
chrmenne <publicdevelopment@christianmenne.de>
2024-11-07 22:29:51 +0100

    Update solarfunctions.cpp
    added github link

--- solarfunctions.cpp
+++ solarfunctions.cpp
@@ -1,6 +1,7 @@
 /*
  * solarfunctions: precise solar calculations
  * Copyright (c) 2024 Christian Menne
+ * https://github.com/chrmenne/solarfunctions
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
